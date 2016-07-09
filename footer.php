093c0dba34e712e5f87b8343924a1ba0fff32d49
matmenzl <info@mathiasmenzl.ch>
2016-07-09 17:39:05 +0200

    added dynamic header title and footer infos

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
       <hr>
  
        <footer>
-        <p>&copy; 2015 Company, Inc.</p>
+        <p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?></p>
       </footer>
     </div> <!-- /container -->
 
 
     <?php wp_footer(); ?>
-    
+
   </body>
 </html>
--- header.php
+++ header.php
@@ -5,11 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+    <link rel="icon" href="<?php bloginfo('template_directory');?>/images/favicon.ico">
 
-    <title>Jumbotron Template for Bootstrap</title>
+    <title>
+
+    <? wp_title('', true, 'right');?>
+    <? bloginfo('name');?>
+      
+    </title>
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
