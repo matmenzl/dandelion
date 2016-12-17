c6f7c358f801c5d08c484c2ac8efcfd6f2e84331
matmenzl <info@mathiasmenzl.ch>
2016-12-17 18:11:23 +0100

    styled team page

--- template-team.php
+++ template-team.php
@@ -10,31 +10,74 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Our Amazing Team</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h2 class="section-heading">Team Schule DANDELION</h2>
+                    <h3 class="section-subheading text-muted">Die Kinder werden bis 16 Uhr immer von einer Lehrperson und mindestens einem Lernbegleiter
+betreut. Nach 16Uhr sind die Lernbegleiter vor Ort.</h3>
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-4">
+            <div class="col-sm-2 col-md-2">
+                <div class="team-member">
+                    <img src="/dandelion/wp-content/themes/dandelion/images/brigitte.jpg" class="img-responsive img-circle" alt="">
+                    <h4>Brigitte Jörg</h4>
+                    <p class="text-muted">Lehrerin</p>
+                    <ul class="list-inline social-buttons">
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-2 col-md-2">
+                <div class="team-member">
+                    <img src="/dandelion/wp-content/themes/dandelion/images/user.png" class="img-responsive img-circle" alt=""></i>
+                    <h4>Renate Jörg</h4>
+                    <p class="text-muted">Englisch-Lehrerin</p>
+                    <ul class="list-inline social-buttons">
+                    </ul>
+                </div>
+            </div>
+                <div class="col-sm-2 col-md-2">
                     <div class="team-member">
-                        <img src="img/team/1.jpg" class="img-responsive img-circle" alt="">
-                        <h4>Kay Garland</h4>
-                        <p class="text-muted">Lead Designer</p>
+                        <img src="/dandelion/wp-content/themes/dandelion/images/caroline.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Caroline Gimpel</h4>
+                        <p class="text-muted">Lernbegleiterin, Gesellschafterin</p>
                         <ul class="list-inline social-buttons">
-                            <li><a href="#"><i class="fa fa-twitter"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-facebook"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-2 col-md-2">
+                    <div class="team-member">
+                        <img src="/dandelion/wp-content/themes/dandelion/images/angela.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Angela Jörg</h4>
+                        <p class="text-muted">Schulleiterin & Lernbegleiterin</p>
+                        <ul class="list-inline social-buttons">
+                            <li><i class="fa fa-envelope"></i><a href="mailto:angela.korner@schule-dandelion.ch"> Email</a>
+                            <li><i class="fa fa-phone"> </i><a href="tel:+41792065543"> Telefon</a>
                             </li>
                         </ul>
                     </div>
                 </div>
+                <div class="col-sm-2 col-md-2 col-lg-2">
+                    <div class="team-member">
+                        <img src="/dandelion/wp-content/themes/dandelion/images/mathias.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Mathias Menzl</h4>
+                        <p class="text-muted">Gesellschafter</p>
+                        <ul class="list-inline social-buttons">
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-2 col-md-2">
+                    <div class="team-member">
+                        <img src="/dandelion/wp-content/themes/dandelion/images/michael.jpeg" class="img-responsive img-circle" alt="">
+                        <h4>Michael Korner</h4>
+                        <p class="text-muted">Gesellschafter</p>
+                        <ul class="list-inline social-buttons">
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="row">
+<!--             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 text-center">
                     <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
                 </div>
-            </div>
+            </div> -->
 
     <?php get_footer() ?>
