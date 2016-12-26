0f53e3c607d6339e559369c460f1155e220820e4
matmenzl <info@mathiasmenzl.ch>
2016-12-26 19:44:18 +0100

    changed team site content and images

--- template-team.php
+++ template-team.php
@@ -12,61 +12,61 @@
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">Team Schule DANDELION</h2>
                     <div class="text section-subheading text-muted">Die Kinder werden bis 16 Uhr immer von einer Lehrperson und mindestens einem Lernbegleiter
-betreut. Nach 16Uhr sind die Lernbegleiter vor Ort.<div>
+betreut. Nach 16 Uhr sind die Lernbegleiterinnen vor Ort.<div>
                 </div>
             </div>
             </div>
             </div>
             <div class="row">
-            <div class="col-sm-3 col-md-3">
+            <div class="col-sm-4 col-md-4">
                 <div class="team-member">
-                    <img src="/wp-content/themes/dandelion/images/brigitte.jpg" class="img-responsive img-circle" alt="">
-                    <h4>Brigitte Jörg</h4>
+                    <img src="/wp-content/themes/dandelion/images/angela.jpg" class="img-responsive img-circle imgteam" alt="">
+                    <h4>Angela Joerg</h4>
+                    <p class="text-muted">Schulleiterin & Lernbegleiterin</p>
+                    <ul class="list-inline social-buttons">
+                        <li><i class="fa fa-envelope"></i><a href="mailto:angela.korner@schule-dandelion.ch"> Email</a>
+                        <li><i class="fa fa-phone"> </i><a href="tel:+41792065543"> Telefon</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="team-member">
+                    <img src="/wp-content/themes/dandelion/images/brigitte.jpg" class="img-responsive img-circle imgteam" alt="">
+                    <h4>Brigitte Joerg</h4>
                     <p class="text-muted">Lehrerin</p>
                     <ul class="list-inline social-buttons">
                     </ul>
                 </div>
             </div>
-            <div class="col-sm-3 col-md-3">
+            <div class="col-sm-4 col-md-4">
                 <div class="team-member">
-                    <img src="/wp-content/themes/dandelion/images/user.png" class="img-responsive img-circle" alt=""></i>
-                    <h4>Renate Jörg</h4>
+                    <img src="/wp-content/themes/dandelion/images/renate.jpg" class="img-responsive img-circle imgteam" alt=""></i>
+                    <h4>Renate Joerg</h4>
                     <p class="text-muted">Englisch-Lehrerin</p>
                     <ul class="list-inline social-buttons">
                     </ul>
                 </div>
             </div>
-                <div class="col-sm-3 col-md-3">
+                <div class="col-sm-4 col-md-4">
                     <div class="team-member">
-                        <img src="/wp-content/themes/dandelion/images/caroline.jpg" class="img-responsive img-circle" alt="">
-                        <h4>Caroline Gimpel</h4>
+                        <img src="/wp-content/themes/dandelion/images/caroline.jpg" class="img-responsive img-circle imgteam" alt="">
+                        <h4>Caroline Gimpel Menzl</h4>
                         <p class="text-muted">Lernbegleiterin</p>
                         <ul class="list-inline social-buttons">
                         </ul>
                     </div>
                 </div>
-                <div class="col-sm-3 col-md-3">
+                <div class="col-sm-4 col-md-4">
                     <div class="team-member">
-                        <img src="/wp-content/themes/dandelion/images/angela.jpg" class="img-responsive img-circle" alt="">
-                        <h4>Angela Jörg</h4>
-                        <p class="text-muted">Schulleiterin & Lernbegleiterin</p>
-                        <ul class="list-inline social-buttons">
-                            <li><i class="fa fa-envelope"></i><a href="mailto:angela.korner@schule-dandelion.ch"> Email</a>
-                            <li><i class="fa fa-phone"> </i><a href="tel:+41792065543"> Telefon</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-<!--                 <div class="col-sm-2 col-md-2 col-lg-2">
-                    <div class="team-member">
-                        <img src="/dandelion/wp-content/themes/dandelion/images/mathias.jpg" class="img-responsive img-circle" alt="">
-                        <h4>Mathias Menzl</h4>
-                        <p class="text-muted">Gesellschafter</p>
+                        <img src="/wp-content/themes/dandelion/images/denise.jpg" class="img-responsive img-circle imgteam" alt="">
+                        <h4>Denise Frehner</h4>
+                        <p class="text-muted">Lehrerin Abenteuer-Nachmittag</p>
                         <ul class="list-inline social-buttons">
                         </ul>
                     </div>
                 </div>
-                <div class="col-sm-2 col-md-2">
+            <!--  <div class="col-sm-2 col-md-2">
                     <div class="team-member">
                         <img src="/dandelion/wp-content/themes/dandelion/images/michael.jpeg" class="img-responsive img-circle" alt="">
                         <h4>Michael Korner</h4>
