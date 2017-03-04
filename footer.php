04431ca47a65ca6483770dee4a84b9749419e4cd
matmenzl <info@mathiasmenzl.ch>
2017-03-04 22:48:02 +0100

    optimized footer

--- footer.php
+++ footer.php
@@ -1,13 +1,22 @@
         <hr>
 
         <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; <?php bloginfo('name');?> <?php echo date('Y');?></p>
+            <footer>
+              <div class="col-sm-12 col-md-12 col-lg-12">
+                <div class="col-sm-6 col-md-6 col-lg-6">
+                  <div class="footerleft">
+                    <div class="copyright">
+                      &copy; <?php echo date("Y"); echo " "; echo bloginfo('name'); ?> 
+                    </div>      
+                  </div> 
                 </div>
-            </div>
-        </footer>
+                <div class="col-sm-6 col-md-6 col-lg-6">
+                  <div class="footerright">
+                    Website by <a href="http://www.mathiasmenzl.ch"><i class="fa fa-heart" aria-hidden="true"></i>|<i class="fa fa-code" aria-hidden="true"></i>|<i class="fa fa-headphones" aria-hidden="true"></i></a>
+                  </div>
+                </div>
+                </div>
+            </footer>
 
     </div>
     <!-- /.container -->
